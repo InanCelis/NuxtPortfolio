cbd4e365bd5721dfa4ff512ce79843d31348f744
inancelis <celisinan@gmail.com>
2021-01-25 11:39:28 +0800

    Last commit

--- pages/skills/index.vue
+++ pages/skills/index.vue
@@ -2,12 +2,11 @@
   <div class="container">
       <div class="row ml-2">
           <div class="h_line shadow"></div>
-          <h5 class="text-center text-md-left text-uppercase font-weight-bold dark-text" >Skills</h5>
+          <h5 class="text-center text-md-left text-uppercase font-weight-bold dark-text" ></h5>
           <span class="page-number dark-text d-none d-xl-block ml-auto">04</span>
 
       </div>
       <div class="row d-flex pb-5" >
-        Inan updated again
       </div>
   </div>
 </template>
