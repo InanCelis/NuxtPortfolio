cc962072e411faf6b5cb97d642c6795f43598e81
inancelis <celisinan@gmail.com>
2021-01-25 11:43:33 +0800

    edit skills

--- pages/skills/index.vue
+++ pages/skills/index.vue
@@ -2,7 +2,7 @@
   <div class="container">
       <div class="row ml-2">
           <div class="h_line shadow"></div>
-          <h5 class="text-center text-md-left text-uppercase font-weight-bold dark-text" ></h5>
+          <h5 class="text-center text-md-left text-uppercase font-weight-bold dark-text" >Skills</h5>
           <span class="page-number dark-text d-none d-xl-block ml-auto">04</span>
 
       </div>
